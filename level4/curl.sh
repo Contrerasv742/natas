50579425d56c645965e736feef4e922634ff1c7d
victor <contrerasv742@gmail.com>
2025-02-01 15:28:00 -0800

    Attempting level 5

--- level4/curl.sh
+++ level4/curl.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
+# Intent: act as if a curl request is coming from natas5 instead of my PC
+
 curl 'http://natas4.natas.labs.overthewire.org' \
   -H 'Referer: http://natas5.natas.labs.overthewire.org/' \
   -u natas4:QryZXc2e0zahULdHrtHxzyYkj59kUxLQ
-#  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-#  -H 'Accept-Language: en-US,en;q=0.9' \
-#  -H 'Authorization: Basic bmF0YXM0OlFyeVpYYzJlMHphaFVMZEhydEh4enlZa2o1OWtVeExR' \
-#  -H 'Connection: keep-alive' \
-#  -H 'Upgrade-Insecure-Requests: 1' \
-#  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36' \
-#  --insecure
---
+++ level5/curl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl 'http://natas5.natas.labs.overthewire.org' \
+    -u natas5:0n35PkggAPm2zbEpOU802c0x0Msn1ToK \
+    -v \
+    -H "Authorization: Basic bmF0YXM1OjBuMzVQa2dnQVBtMnpiRXBPVTgwMmMweDBNc24xVG9L"
